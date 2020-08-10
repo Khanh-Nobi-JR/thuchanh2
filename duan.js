03b2b1876fcbf5e85dbbcebdfc59e31fedaa6b42
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-08-10 12:16:27 +0700

    add toán tử

---
+++ duan.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("nhập chiều rộng");
+inputheight = prompt("nhập chiều cao");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write("the area is: " + area);
